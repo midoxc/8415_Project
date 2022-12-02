d800b75d4497dc1ebf70a23f8725bab2ba612b9f
midoxc <minh-tri.do@polymtl.ca>
2022-12-02 01:20:45 -0500

    fixed proxy service

--- app.py
+++ app.py
@@ -6,17 +6,16 @@ import paramiko
 import random
 from flask import Flask, request
 from pythonping import ping
-import os
 
 app = Flask(__name__)
 
 key = paramiko.RSAKey.from_private_key_file("vockey.pem")
 
 ips = {
-    'master'    : 'ubuntu@ip-172-31-1-1.ec2.internal',
-    'worker1'   : 'ubuntu@ip-172-31-1-2.ec2.internal',
-    'worker2'   : 'ubuntu@ip-172-31-1-3.ec2.internal',
-    'worker3'   : 'ubuntu@ip-172-31-1-4.ec2.internal'
+    'master'    : 'ip-172-31-1-1.ec2.internal',
+    'worker1'   : 'ip-172-31-1-2.ec2.internal',
+    'worker2'   : 'ip-172-31-1-3.ec2.internal',
+    'worker3'   : 'ip-172-31-1-4.ec2.internal'
 }
 
 workers = ['worker1','worker2','worker3']
@@ -25,9 +24,9 @@ def getLowestPingWorker():
     best = ""
     time = 2000
     for worker in workers:
-        ping_result = ping(ips[worker], count=4, timeout=2)
-        if ping_result.packet_loss != 4 and time > ping_result.rtt_avg_ms:
-            worker = best
+        ping_result = ping(ips[worker], count=1, timeout=2)
+        if ping_result.packet_loss != 1 and time > ping_result.rtt_avg_ms:
+            best = worker
             time = ping_result.rtt_avg_ms
 
     return best
@@ -35,31 +34,30 @@ def getLowestPingWorker():
 def needsWriteAccess(query):
     instructions = query.split(";")
     for instruction in instructions:
-        keyword = instruction.strip().split()[0].lower()
-        if keyword in ["delete", "update", "create", "insert", "grant", "revoke"]:
+        keyword = instruction.strip().lower().split()
+        if len(keyword) > 0 and keyword[0] in ["delete", "update", "create", "insert", "grant", "revoke"]:
             return True
+    return False
 
 def formatQuery(query):
-    query.replace('"', '\"')
-    query.replace('$', '\$')
-    query.replace('`', '\`')
+    query = query.replace('"', '\\"')
+    query = query.replace('$', '\\$')
+    query = query.replace('`', '\\`')
     return query
 
 def executeCommands(name, commands):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname=ips[name], pkey=key, allow_agent=False, look_for_keys=False)
+    ssh.connect(hostname=ips[name], username="ubuntu", pkey=key, allow_agent=False, look_for_keys=False)
 
-    output = ""
+    output = name + "\n"
     for command in commands:
         print("{} running command: {}".format(name, command))
         stdin , stdout, stderr = ssh.exec_command(command)
         out = stdout.read()
         err = stderr.read()
-        print(os.popen("echo " + out).read())
-        print(err)
-        output += out.decode("ISO-8859-1") + "\n"
-        output += err.decode("ISO-8859-1") + "\n"
+        output += out.decode("latin-1") + "\n"
+        output += err.decode("latin-1") + "\n"
 
     ssh.close()
 
@@ -67,11 +65,13 @@ def executeCommands(name, commands):
 
 def direct(query):
     print("sudo mysql -u root -e \"{}\"".format(formatQuery(query)))
-    return executeCommands("master", ["sudo mysql -u root -e \"{}\"".format(formatQuery(query))])
+    return executeCommands("master", ["sudo mysql -e \"{}\"".format(formatQuery(query))])
 
 def randomized(query):
     if not needsWriteAccess(query):
-        return executeCommands(random.choice(workers), ["sudo mysql -u root -e \"{}\"".format(formatQu$
+        random_worker = random.choice(workers)
+        print("read on " + random_worker)
+        return executeCommands(random_worker, ["sudo mysql -e \"{}\"".format(formatQuery(query))])
 
     return direct(query)
 
@@ -79,7 +79,8 @@ def customized(query):
     if not needsWriteAccess(query):
         fastestWorker = getLowestPingWorker()
         if fastestWorker != "":
-            return executeCommands(fastestWorker, ["sudo mysql -u root -e \"{}\"".format(formatQuery(q$
+            print("read on " + fastestWorker)
+            return executeCommands(fastestWorker, ["sudo mysql -e \"{}\"".format(formatQuery(query))])
 
     return direct(query)
 
@@ -94,15 +95,15 @@ def handleRequest(path=None):
         return "INVALID POST PARAMS : missing type or query \n" + request.json
 
     print(request.json)
-    type = str(type)
-    query = str(query)
-    print()
+    type = str(type).strip()
+    query = str(query).strip()
+
     if type == "custom":
-        return customized(query)
+        return customized(query).strip().encode("latin-1")
     elif type == "random":
-        return randomized(query)
+        return randomized(query).strip().encode("latin-1")
     else:
-        return direct(query)
+        return direct(query).strip().encode("latin-1")
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=8000)
